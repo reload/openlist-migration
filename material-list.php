e8a51d96ba6bc97f05c7d78bd6f0f91487138271
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-10-25 15:00:16 +0200

    Document steps in material list migration

--- material-list.php
+++ material-list.php
@@ -53,38 +53,51 @@ SQL
       , ['connection' => 'openlist'])
     ->transform('callback', [
       'callback' => function (Row $row) {
+        // Prefix owner names with legacy-. This is required by materiallist
+        // as a way to identify users waiting to be migrated.
         $owner = $row->get('owner');
         $row->set('owner', 'legacy-' . $owner);
       }
     ])
     ->transform('callback', [
       'callback' => function (Row $row) {
+        // The default list name is "default"
         $row->set('list', 'default');
       }
     ])
     ->transform('callback', [
       'callback' => function (Row $row) {
+        // Extract data consisting of serialized PHP from each element.
         $data = unserialize($row->get('data'));
-        if ($data['type'] && $data['type'] !== 'ting_object') {
+        // We only care about elements referring to materials. Discard
+        // other types which might have found their way in here.
+        if (!empty($data['type'])
+          && $data['type'] !== 'ting_object') {
           var_dump($row->toArray());
           $row->discard();
         }
 
+        // Discard elements without a material id.
         if (empty($data['value'])) {
           var_dump($row->toArray());
           $row->discard();
-        } else {
-          $row->set('data', $data['value']);
         }
+
+        $row->set('data', $data['value']);
       }
     ])
+    // Only allow one of each material on the list. As we migrate from multiple
+    // lists to one it is important that we do not include duplicates.
     ->transform('unique_rows', ['columns' => ['owner', 'list', 'data']])
+    // Register the row for debugging. If an database error occurs it can be
+    // hard to tell what was going to be inserted. Here it is nice to have
+    // the actual row available.
     ->transform('callback', [
-      'callback' => function (Row $row) {
-        global $last_row;
+      'callback' => function (Row $row) use (&$last_row) {
         $last_row = $row;
       }
     ])
+
     ->load('insert', 'materials', [
       'columns' => [
         'owner' => 'guid',
